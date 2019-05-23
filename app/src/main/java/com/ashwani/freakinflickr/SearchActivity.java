3f166f22fde8a29bfcbb10e1c082ceda241d9ff6
Ashwani Pandey <ashwanipandey1218@gmail.com>
2019-05-23 13:32:51 +0530

    Implemented Search Facility to search images based on Tags.

--- app/src/main/java/com/ashwani/freakinflickr/SearchActivity.java
+++ app/src/main/java/com/ashwani/freakinflickr/SearchActivity.java
@@ -1,8 +1,19 @@
 package com.ashwani.freakinflickr;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.SearchView;
 
 public class SearchActivity extends BaseActivity {
+
+    private static final String TAG = "SearchActivity";
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +23,43 @@ public class SearchActivity extends BaseActivity {
         activateToolbar(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
+
+        searchView.setIconified(false);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit: called");
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                sharedPreferences.edit().putString(FLICKR_QUERY, query).apply();
+                searchView.clearFocus();
+                finish();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                finish();
+                return false;
+            }
+        });
+
+        Log.d(TAG, "onCreateOptionsMenu: returned " + true);
+        return true;
+    }
 }
